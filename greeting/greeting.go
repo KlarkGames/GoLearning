7d8529ff98f2afa77a27afe7ad3c107af3e142cb
KlarkGames <laletin.alesha@mail.ru>
2023-02-21 14:00:46 +0300

    Random greeting added

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -1,8 +1,26 @@
 package greeting
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Greeting(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Greeting(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("no name found")
+	}
+
+	message := fmt.Sprintf(randFormat(), name)
+	return message, nil
+}
+
+func randFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Hello, %v. Nice to meet you here!",
+		"Здравствуй, %v. Рад тебя видеть",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- main.go
+++ main.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"github.con/KlarkGames/GoLearning/greeting"
+	"log"
+	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println(greeting.Greeting("Alex"))
+	message, err := greeting.Greeting("Алексей")
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println(message)
+	fmt.Println(quote.Go())
 }
